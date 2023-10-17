8682e35be56c16e607529eb6dfdc592363a89cc5
Gabriel <102957524+gabri7sc@users.noreply.github.com>
2023-10-17 19:39:28 -0300

    Update handler.py
    Returning the originial code

--- handler.py
+++ handler.py
@@ -10,44 +10,39 @@ model = pickle.load( open( 'model/model_linear_regression.pkl', 'rb' ) )
 # initialize API
 app = Flask(__name__)
 
-@app.route( '/healthinsurance/predict', methods=['POST'] )
-def healthinsurance_predict():
+
+@app.route( '/healthInsurance/predict', methods=['GET', 'POST'])
+def health_insurance_predict():
     test_json = request.get_json()
-   
-    if test_json: # there is data
-        if isinstance( test_json, dict ): # unique example
+
+    if test_json:  # there is data
+        if isinstance( test_json, dict ):  # unique example
             test_raw = pd.DataFrame( test_json, index=[0] )
-            
-        else: # multiple example
+
+        else:  # multiple example
             test_raw = pd.DataFrame( test_json, columns=test_json[0].keys() )
-            
+
+        # Instantiate Rossmann class
         pipeline = HealthInsurance()
 
-        #Data Description
-        df1 = pipeline.rename_columns(test_raw)
+        # data cleaning
+        df1 = pipeline.data_cleaning( test_raw )
 
-        #Data Transformation
-        df2 = pipeline.rename_categorical(df1)
+        # feature engineering
+        df2 = pipeline.feature_engineering( df1 )
 
-        #Data Encoding
-        df3 = pipeline.data_encoding(df2)
+        # data preparation
+        df3 = pipeline.data_preparation( df2 )
 
-        #Data Rescalling
-        df4 = pipeline.data_rescalling(df3)
+        # prediction
+        df_response = pipeline.get_prediction( model, test_raw, df3 )
 
-        #prediction
-        df_response = pipeline.get_prediction(model, test_raw, df4)
-        
         return df_response
-        
-        
+
     else:
         return Response( '{}', status=200, mimetype='application/json' )
-    
-#Usar isso para testar na maquina
-#if __name__ == '__main__':
-    #app.run('0.0.0.0', debug=True)
 
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    app.run(host='0.0.0.0', port=port)
+    port = os.environ.get( 'PORT', 5000 )
+    app.run( host='0.0.0.0', port=port, debug=True )
+
