d955348c1d8a6971921faf9344127ce997146e9e
Gabriel <102957524+gabri7sc@users.noreply.github.com>
2023-10-17 19:18:53 -0300

    Update HealthInsurance.py
    Trying another class

--- healthInsurance/HealthInsurance.py
+++ healthInsurance/HealthInsurance.py
@@ -1,75 +1,91 @@
 import pickle
-import numpy as np
 import pandas as pd
+import numpy as np
+import inflection
 
-class HealthInsurance( object ):
+class HealthInsurance:
     def __init__(self):
-        self.home_path = ''
-        self.annual_premium_scaler            = pickle.load( open( self.home_path + 'parameter/annual_premium_scaler.pkl', 'rb' ) )
-        self.age_scaler                       = pickle.load( open( self.home_path + 'parameter/age_scaler.pkl', 'rb' ) )
-        self.vintage_scaler                   = pickle.load( open( self.home_path + 'parameter/vintage_scaler.pkl', 'rb' ) )
-        self.target_encode_gender_scaler      = pickle.load( open( self.home_path + 'parameter/target_encode_gender_scaler.pkl', 'rb' ) )
-        self.target_encode_region_code_scaler = pickle.load( open( self.home_path + 'parameter/target_encode_region_code_scaler.pkl', 'rb' ) )
-        self.fe_policy_sales_channel_scaler   = pickle.load( open( self.home_path + 'parameter/fe_policy_sales_channel_scaler.pkl', 'rb' ) )
-
-    def data_cleaning( self, df1 ):
-
-        # 1.1. Rename Columns
-        cols_new = ['id', 'gender', 'age', 'driving_license', 'region_code', 'previously_insured', 'vehicle_age',
-                    'vehicle_damage', 'annual_premium','policy_sales_channel', 'vintage', 'response']
+        self.home_path                = ''
         
-        # rename
+        self.age_scaler                           = pickle.load(open(self.home_path + 'features/age_scaler.pkl', 'rb'))
+        self.annual_premium_scaler                = pickle.load(open(self.home_path + 'features/annual_premium_scaler.pkl', 'rb'))        
+        self.fe_policy_sales_channel_scaler       = pickle.load(open(self.home_path + 'features/fe_policy_sales_channel_scaler.pkl', 'rb'))        
+        self.fe_region_code_scaler                = pickle.load(open(self.home_path + 'features/fe_region_code_scaler.pkl', 'rb'))        
+        self.vintage_scaler                       = pickle.load(open(self.home_path + 'features/vintage_scaler.pkl', 'rb'))        
+        
+    def rename_columns(self, df1):
+        cols_old = df1.columns
+
+        cols_new = []
+        cols_new = cols_old.map(lambda x: inflection.underscore(x))
+
         df1.columns = cols_new
 
         return df1
+    
+    def rename_categorical(self, df2):
 
-    def feature_engineering( self, df2 ):
-        # 2.0. Feature Engineering
+        #Renaming vehicle_age lines
+        #< 1 Year = new || 1-2 Years = used || >2 Years = old
+        df2['vehicle_age'] = df2['vehicle_age'].apply(lambda x: 'old' if x == '> 2 Years' else 'used' if x == '1-2 Year' else 'new')
 
-        # Vehicle Damage Number
-        df2['vehicle_damage'] = df2['vehicle_damage'].apply( lambda x: 1 if x == 'Yes' else 0 )
+        #Renaming vehicle_damage
+        df2['vehicle_damage'] = df2['vehicle_damage'].apply(lambda x: 1 if x=='Yes' else 0)
 
-        # Vehicle Age
-        df2['vehicle_age'] = df2['vehicle_age'].apply(
-            lambda x: 'over_2_years' if x == '> 2 Years' else 'between_1_2_year' if x == '1-2 Year' else 'below_1_year' )
-
+        #Renaming gender #Male = 1, Female = 0
+        df2['gender'] = df2['gender'].apply(lambda x: 1 if x=='Male' else 0)
+    
         return df2
+
+    def data_encoding(self, df5):
+
+        #vehicle_age - Label Encoder
+        df5['vehicle_age'] = df5['vehicle_age'].apply(lambda x: 0 if x == 'new' else 1 if x == 'used' else 2)
+
+        #region_code - Frequency Encoding
+        df5['region_code'] = df5['region_code'].map(self.fe_region_code_scaler)
+        #pickle.dump(fe_region_code, open('C:/Users/edils/repos/pa004_health_insurance/src/features/fe_region_code_scaler.pkl','wb'))
+
+        #policy_sales_channel
+        df5['policy_sales_channel'] = df5['policy_sales_channel'].map(self.fe_policy_sales_channel_scaler)
+        #pickle.dump(fe_policy_sales_channel, open('C:/Users/edils/repos/pa004_health_insurance/src/features/fe_policy_sales_channel_scaler.pkl', 'wb'))
+
+        return df5
         
-    def data_preparation( self, df5 ):
+    def data_rescalling(self, df5):
+
+        #age 
+        df5['age'] = self.age_scaler.transform(df5['age'].values.reshape(-1,1))
+        #pickle.dump(mms, open('C:/Users/edils/repos/pa004_health_insurance/src/features/age_scaler.pkl', 'wb'))
+
+
+        #annual_premium
+        df5['annual_premium'] = self.annual_premium_scaler.transform(df5['annual_premium'].values.reshape(-1,1))
+        #pickle.dump(rb, open('C:/Users/edils/repos/pa004_health_insurance/src/features/annual_premium_scaler.pkl', 'wb'))
+
+
+        #vintage
+        df5['vintage'] = self.vintage_scaler.transform(df5['vintage'].values.reshape(-1,1))
+        #pickle.dump(mms, open('C:/Users/edils/repos/pa004_health_insurance/src/features/vintage_scaler.pkl', 'wb'))
         
-        # anual premium - StandarScaler
-        df5['annual_premium'] = self.annual_premium_scaler.transform( df5[['annual_premium']].values )
-
-        # Age - MinMaxScaler
-        df5['age'] = self.age_scaler.transform( df5[['age']].values )
-
-        # Vintage - MinMaxScaler
-        df5['vintage'] = self.vintage_scaler.transform( df5[['vintage']].values )
-
-        # gender - One Hot Encoding / Target Encoding
-        df5.loc[:, 'gender'] = df5['gender'].map( self.target_encode_gender_scaler )
-
-        # region_code - Target Encoding / Frequency Encoding
-        df5.loc[:, 'region_code'] = df5['region_code'].map( self.target_encode_region_code_scaler )
-
-        # vehicle_age - One Hot Encoding / Frequency Encoding
-        df5 = pd.get_dummies( df5, prefix='vehicle_age', columns=['vehicle_age'] )
-
-        # policy_sales_channel - Target Encoding / Frequency Encoding
-        df5.loc[:, 'policy_sales_channel'] = df5['policy_sales_channel'].map( self.fe_policy_sales_channel_scaler )
-
-        # Feature Selection
-        cols_selected = ['annual_premium', 'vintage', 'age', 'region_code', 'vehicle_damage', 'previously_insured',
-                         'policy_sales_channel']
+        cols_selected = {'vintage',
+                         'annual_premium',
+                         'age',
+                         'region_code',
+                         'vehicle_damage',
+                         'policy_sales_channel',
+                         'previously_insured',
+                         'vehicle_age',
+                         'gender'}
 
         return df5[cols_selected]
+                  
 
-    def get_prediction( self, model, original_data, test_data ):
-
-        # model prediction
-        pred = model.predict_proba( test_data )
-
-        # join prediction into original data
-        original_data['prediction'] = pred
-
-        return original_data.to_json( orient='records', date_format='iso' )
+    def get_prediction(self, model, original_data, test_data):
+        
+        yhat = model.predict_proba(test_data)
+        
+        original_data['Prediction'] = yhat[:,1]
+        
+        return original_data.to_json(orient='records', date_format='iso')
+        
